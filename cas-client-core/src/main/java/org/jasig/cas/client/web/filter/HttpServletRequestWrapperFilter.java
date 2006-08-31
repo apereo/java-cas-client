20ba0578e8ea30c6db69a5e108fa7580bd795524
Scott Battaglia <scott.battaglia@gmail.com>
2006-08-31 04:00:06 +0000

    CASC-7 updated to put redirect within code that looks for ticket

--- cas-client-core/src/main/java/org/jasig/cas/client/web/filter/HttpServletRequestWrapperFilter.java
+++ cas-client-core/src/main/java/org/jasig/cas/client/web/filter/HttpServletRequestWrapperFilter.java
@@ -9,16 +9,9 @@ import org.jasig.cas.client.validation.Assertion;
 import org.springframework.web.util.WebUtils;
 
 import javax.servlet.*;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.io.BufferedReader;
+import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.Principal;
-import java.util.Enumeration;
-import java.util.Locale;
-import java.util.Map;
 
 /**
  * Implementation of a filter that wraps the normal HttpServletRequest with a
@@ -54,65 +47,14 @@ public final class HttpServletRequestWrapperFilter implements Filter {
         // nothing to do
     }
 
-    final class CasHttpServletRequestWrapper implements
-            HttpServletRequest {
-
-        private final HttpServletRequest request;
+    final class CasHttpServletRequestWrapper extends HttpServletRequestWrapper {
 
         CasHttpServletRequestWrapper(final HttpServletRequest request) {
-            this.request = request;
-        }
-
-        public String getAuthType() {
-            return this.request.getAuthType();
-        }
-
-        public Cookie[] getCookies() {
-            return this.request.getCookies();
-        }
-
-        public long getDateHeader(String s) {
-            return this.request.getDateHeader(s);
-        }
-
-        public String getHeader(String s) {
-            return this.request.getHeader(s);
-        }
-
-        public Enumeration getHeaders(String s) {
-            return this.request.getHeaders(s);
-        }
-
-        public Enumeration getHeaderNames() {
-            return this.request.getHeaderNames();
-        }
-
-        public int getIntHeader(String s) {
-            return this.request.getIntHeader(s);
-        }
-
-        public String getMethod() {
-            return this.request.getMethod();
-        }
-
-        public String getPathInfo() {
-            return this.request.getPathInfo();
-        }
-
-        public String getPathTranslated() {
-            return this.request.getPathTranslated();
-        }
-
-        public String getContextPath() {
-            return this.request.getContextPath();
-        }
-
-        public String getQueryString() {
-            return this.request.getQueryString();
+            super(request);
         }
 
         public String getRemoteUser() {
-            final org.jasig.cas.authentication.principal.Principal p = (org.jasig.cas.authentication.principal.Principal) this.request
+            final org.jasig.cas.authentication.principal.Principal p = (org.jasig.cas.authentication.principal.Principal) this
                     .getAttribute(AbstractCasFilter.CONST_PRINCIPAL);
 
             if (p != null) {
@@ -120,7 +62,7 @@ public final class HttpServletRequestWrapperFilter implements Filter {
             }
 
             final Assertion assertion = (Assertion) WebUtils
-                    .getSessionAttribute(this.request,
+                    .getSessionAttribute(this,
                             AbstractCasFilter.CONST_ASSERTION);
 
             if (assertion != null) {
@@ -128,171 +70,6 @@ public final class HttpServletRequestWrapperFilter implements Filter {
             }
 
             return null;
-        }
-
-        public boolean isUserInRole(String s) {
-            return this.request.isUserInRole(s);
-        }
-
-        public Principal getUserPrincipal() {
-            return this.request.getUserPrincipal();
-        }
-
-        public String getRequestedSessionId() {
-            return this.request.getRequestedSessionId();
-        }
-
-        public String getRequestURI() {
-            return this.request.getRequestURI();
-        }
-
-        public StringBuffer getRequestURL() {
-            return this.request.getRequestURL();
-        }
-
-        public String getServletPath() {
-            return this.request.getServletPath();
-        }
-
-        public HttpSession getSession(boolean b) {
-            return this.request.getSession(b);
-        }
-
-        public HttpSession getSession() {
-            return this.request.getSession();
-        }
-
-        public boolean isRequestedSessionIdValid() {
-            return this.request.isRequestedSessionIdValid();
-        }
-
-        public boolean isRequestedSessionIdFromCookie() {
-            return this.request.isRequestedSessionIdFromCookie();
-        }
-
-        public boolean isRequestedSessionIdFromURL() {
-            return this.request.isRequestedSessionIdFromURL();
-        }
-
-        public boolean isRequestedSessionIdFromUrl() {
-            return this.request.isRequestedSessionIdFromUrl();
-        }
-
-        public Object getAttribute(String s) {
-            return this.request.getAttribute(s);
-        }
-
-        public Enumeration getAttributeNames() {
-            return this.request.getAttributeNames();
-        }
-
-        public String getCharacterEncoding() {
-            return this.request.getCharacterEncoding();
-        }
-
-        public void setCharacterEncoding(String s)
-                throws UnsupportedEncodingException {
-            this.request.setCharacterEncoding(s);
-        }
-
-        public int getContentLength() {
-            return this.request.getContentLength();
-        }
-
-        public String getContentType() {
-            return this.request.getContentType();
-        }
-
-        public ServletInputStream getInputStream() throws IOException {
-            return this.request.getInputStream();
-        }
-
-        public String getParameter(String s) {
-            return this.request.getParameter(s);
-        }
-
-        public Enumeration getParameterNames() {
-            return this.request.getParameterNames();
-        }
-
-        public String[] getParameterValues(String s) {
-            return this.request.getParameterValues(s);
-        }
-
-        public Map getParameterMap() {
-            return this.request.getParameterMap();
-        }
-
-        public String getProtocol() {
-            return this.request.getProtocol();
-        }
-
-        public String getScheme() {
-            return this.request.getScheme();
-        }
-
-        public String getServerName() {
-            return this.request.getServerName();
-        }
-
-        public int getServerPort() {
-            return this.request.getServerPort();
-        }
-
-        public BufferedReader getReader() throws IOException {
-            return this.request.getReader();
-        }
-
-        public String getRemoteAddr() {
-            return this.request.getRemoteAddr();
-        }
-
-        public String getRemoteHost() {
-            return this.request.getRemoteHost();
-        }
-
-        public void setAttribute(String s, Object o) {
-            this.request.setAttribute(s, o);
-        }
-
-        public void removeAttribute(String s) {
-            this.request.removeAttribute(s);
-        }
-
-        public Locale getLocale() {
-            return this.request.getLocale();
-        }
-
-        public Enumeration getLocales() {
-            return this.request.getLocales();
-        }
-
-        public boolean isSecure() {
-            return this.request.isSecure();
-        }
-
-        public RequestDispatcher getRequestDispatcher(String s) {
-            return this.request.getRequestDispatcher(s);
-        }
-
-        public String getRealPath(String s) {
-            return this.request.getRealPath(s);
-        }
-
-        public int getRemotePort() {
-            return this.request.getRemotePort();
-        }
-
-        public String getLocalName() {
-            return this.request.getLocalName();
-        }
-
-        public String getLocalAddr() {
-            return this.request.getLocalAddr();
-        }
-
-        public int getLocalPort() {
-            return this.request.getLocalPort();
         }
     }
 }
