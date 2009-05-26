1a11fdd3bd990aa24cd50fda68b91324f12bcaad
Scott Battaglia <scott.battaglia@gmail.com>
2009-05-26 17:52:35 +0000

    NOJIRA
    tagged for 3.1.6

--- cas-client-core/src/test/java/org/jasig/cas/client/validation/Cas20ProxyReceivingTicketValidationFilterTest.java
+++ cas-client-core/src/test/java/org/jasig/cas/client/validation/Cas20ProxyReceivingTicketValidationFilterTest.java
@@ -2,12 +2,119 @@ package org.jasig.cas.client.validation;
 
 import junit.framework.TestCase;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.jasig.cas.client.proxy.CleanUpTimerTask;
+import org.jasig.cas.client.proxy.ProxyGrantingTicketStorage;
+import org.jasig.cas.client.proxy.ProxyGrantingTicketStorageImpl;
+import org.jasig.cas.client.util.MethodFlag;
+
 /**
  * Unit test for {@link Cas20ProxyReceivingTicketValidationFilter}
  * 
  * @author Brad Cupit (brad [at] lsu {dot} edu)
  */
 public class Cas20ProxyReceivingTicketValidationFilterTest extends TestCase {
+
+    private final Timer defaultTimer = new Timer(true);
+
+    private final ProxyGrantingTicketStorage storage = new ProxyGrantingTicketStorageImpl();
+
+    private final CleanUpTimerTask defaultTimerTask = new CleanUpTimerTask(storage);
+
+    public void testStartsThreadAtStartup() throws Exception {
+        final MethodFlag scheduleMethodFlag = new MethodFlag();
+        final Cas20ProxyReceivingTicketValidationFilter filter = newCas20ProxyReceivingTicketValidationFilter();
+
+        final Timer timer = new Timer(true) {
+            public void schedule(TimerTask task, long delay, long period) {
+                scheduleMethodFlag.setCalled();
+            }
+        };
+
+        filter.setMillisBetweenCleanUps(1);
+        filter.setProxyGrantingTicketStorage(storage);
+        filter.setTimer(timer);
+        filter.setTimerTask(defaultTimerTask);
+
+        filter.init();
+        assertTrue(scheduleMethodFlag.wasCalled());
+    }
+
+    public void testShutsDownTimerThread() throws Exception {
+        final MethodFlag cancelMethodFlag = new MethodFlag();
+        final Cas20ProxyReceivingTicketValidationFilter filter = newCas20ProxyReceivingTicketValidationFilter();
+
+        final Timer timer = new Timer(true) {
+            public void cancel() {
+                cancelMethodFlag.setCalled();
+                super.cancel();
+            }
+        };
+
+        filter.setProxyGrantingTicketStorage(storage);
+        filter.setMillisBetweenCleanUps(1);
+        filter.setTimer(timer);
+        filter.setTimerTask(defaultTimerTask);
+        filter.init();
+        filter.destroy();
+        
+        assertTrue(cancelMethodFlag.wasCalled());
+    }
+
+public void testCallsCleanAllOnSchedule() throws Exception {
+        final MethodFlag timerTaskFlag = new MethodFlag();
+        final Cas20ProxyReceivingTicketValidationFilter filter = newCas20ProxyReceivingTicketValidationFilter();
+
+        final TimerTask timerTask = new TimerTask() {
+            public void run() {
+                timerTaskFlag.setCalled();
+            }
+        };
+
+        final int millisBetweenCleanUps = 250;
+        filter.setProxyGrantingTicketStorage(storage);
+        filter.setTimerTask(timerTask);
+        filter.setTimer(defaultTimer);
+        filter.setMillisBetweenCleanUps(millisBetweenCleanUps);
+
+        filter.init();
+
+        // wait long enough for the clean up to occur
+        Thread.sleep(millisBetweenCleanUps * 2);
+
+        assertTrue(timerTaskFlag.wasCalled());
+        filter.destroy();
+    }
+
+    public void testDelaysFirstCleanAll() throws Exception {
+        final MethodFlag timerTaskFlag = new MethodFlag();
+        final Cas20ProxyReceivingTicketValidationFilter filter = newCas20ProxyReceivingTicketValidationFilter();
+
+        final TimerTask timerTask = new TimerTask() {
+            public void run() {
+                timerTaskFlag.setCalled();
+            }
+        };
+
+        final int millisBetweenCleanUps = 250;
+        filter.setProxyGrantingTicketStorage(storage);
+        filter.setMillisBetweenCleanUps(millisBetweenCleanUps);
+        filter.setTimer(defaultTimer);
+        filter.setTimerTask(timerTask);
+
+        filter.init();
+
+        assertFalse(timerTaskFlag.wasCalled());
+
+        // wait long enough for the clean up to occur
+        Thread.sleep(millisBetweenCleanUps * 2);
+
+        assertTrue(timerTaskFlag.wasCalled());
+
+        filter.destroy();
+    }
     
     public void testThrowsForNullStorage() throws Exception {
         Cas20ProxyReceivingTicketValidationFilter filter = newCas20ProxyReceivingTicketValidationFilter();
