b4367f595e8e1ec4677b8f9aac8345f55c45ccbf
Scott Battaglia <scott.battaglia@gmail.com>
2013-01-15 22:04:16 -0500

    Fix formatting of pom.xml and update licensing to match CAS project.

--- cas-client-core/src/main/java/org/jasig/cas/client/validation/AbstractUrlBasedTicketValidator.java
+++ cas-client-core/src/main/java/org/jasig/cas/client/validation/AbstractUrlBasedTicketValidator.java
@@ -1,22 +1,21 @@
-/**
+/*
  * Licensed to Jasig under one or more contributor license
  * agreements. See the NOTICE file distributed with this work
  * for additional information regarding copyright ownership.
  * Jasig licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a
- * copy of the License at:
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.jasig.cas.client.validation;
 
 import org.jasig.cas.client.util.CommonUtils;
@@ -192,8 +191,6 @@ public abstract class AbstractUrlBasedTicketValidator implements TicketValidator
     protected abstract String retrieveResponseFromServer(URL validationUrl, String ticket);
 
     public final Assertion validate(final String ticket, final String service) throws TicketValidationException {
-
-
         final String validationUrl = constructValidationUrl(ticket, service);
          logger.debug("Constructing validation url: {}", validationUrl);
 
