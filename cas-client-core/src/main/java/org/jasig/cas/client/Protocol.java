035a63fe8fdea347ec0aeed122f477b42b81b809
Scott Battaglia <scott.battaglia@gmail.com>
2014-11-20 15:58:52 -0500

    Added missing javadoc for Protocol enumeration

--- cas-client-core/src/main/java/org/jasig/cas/client/Protocol.java
+++ cas-client-core/src/main/java/org/jasig/cas/client/Protocol.java
@@ -1,13 +1,14 @@
 package org.jasig.cas.client;
 
 /**
- * Created by battags on 10/14/14.
+ * Simple enumeration to hold/capture some of the standard request parameters used by the various protocols.
+ *
+ * @author Scott Battaglia
+ * @since 3.4.0
  */
 public enum Protocol {
 
-
-
-    CAS1("ticket", "service"), CAS2(CAS1.getArtifactParameterName(), CAS1.getServiceParameterName()), SAML11("SAMLart", "TARGET");
+    CAS1("ticket", "service"), CAS2(CAS1), SAML11("SAMLart", "TARGET");
 
     private final String artifactParameterName;
 
@@ -16,6 +17,10 @@ public enum Protocol {
     private Protocol(final String artifactParameterName, final String serviceParameterName) {
         this.artifactParameterName = artifactParameterName;
         this.serviceParameterName = serviceParameterName;
+    }
+
+    private Protocol(final Protocol protocol) {
+        this(protocol.getArtifactParameterName(), protocol.getServiceParameterName());
     }
 
     public String getArtifactParameterName() {
