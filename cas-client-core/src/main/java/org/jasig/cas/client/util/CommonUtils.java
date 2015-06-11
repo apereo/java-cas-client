d644cdfc20fcf19cbd16cc178a02e2cbec941ab9
Misagh Moayyed <mmoayyed@unicon.net>
2015-06-10 20:09:15 -0700

    removed since tags

--- cas-client-core/src/main/java/org/jasig/cas/client/util/CommonUtils.java
+++ cas-client-core/src/main/java/org/jasig/cas/client/util/CommonUtils.java
@@ -631,7 +631,6 @@ public final class CommonUtils {
      * @param str  the string to convert, may be null
      * @param defaultValue  the default value
      * @return the long represented by the string, or the default if conversion fails
-     * @since 2.1
      */
     public static long toLong(final String str, final long defaultValue) {
         if (str == null) {
@@ -659,7 +658,6 @@ public final class CommonUtils {
      * @param str  the string to convert, may be null
      * @param defaultValue  the default value
      * @return the int represented by the string, or the default if conversion fails
-     * @since 2.1
      */
     public static int toInt(final String str, final int defaultValue) {
         if(str == null) {
