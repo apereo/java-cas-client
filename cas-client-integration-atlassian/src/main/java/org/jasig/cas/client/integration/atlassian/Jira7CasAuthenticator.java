06a18f92acd0a0ca6dd357d681965f71c2bd857c
Jason Hitt <jasonh@altertrading.com>
2016-12-13 06:51:49 -0600

    Reformatted to Apereo formatting standards.

--- cas-client-integration-atlassian/src/main/java/org/jasig/cas/client/integration/atlassian/Jira7CasAuthenticator.java
+++ cas-client-integration-atlassian/src/main/java/org/jasig/cas/client/integration/atlassian/Jira7CasAuthenticator.java
@@ -41,8 +41,8 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterators;
 
 /**
- * Extension of JiraSeraphAuthenticator to allow people to configure 
- * JIRA 7 and above to authenticate via Jasig CAS
+ * Extension of JiraSeraphAuthenticator to allow people to configure JIRA 7 and
+ * above to authenticate via Jasig CAS
  *
  * @author Scott Battaglia
  * @author Martin Stiborsky
@@ -51,74 +51,60 @@ import com.google.common.collect.Iterators;
  * @version $Revision$ $Date$
  * @since 3.4.2
  */
-public final class Jira7CasAuthenticator extends JiraSeraphAuthenticator
-{
-    private static final long                         serialVersionUID = -8262305967991752189L;
+public final class Jira7CasAuthenticator extends JiraSeraphAuthenticator {
+    private static final long serialVersionUID = -8262305967991752189L;
 
-    private static final Logger                       LOGGER           = LoggerFactory
-            .getLogger(Jira7CasAuthenticator.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Jira7CasAuthenticator.class);
 
-    /** We're just going to be wrapping the functionality of the servlet filter */
+    /**
+     * We're just going to be wrapping the functionality of the servlet filter
+     */
     private Cas20ProxyReceivingTicketValidationFilter validationFilter;
 
     @Override
-    public void init(final Map<String, String> params, final SecurityConfig config)
-    {
+    public void init(final Map<String, String> params, final SecurityConfig config) {
         super.init(params, config);
 
-        try
-        {
+        try {
             validationFilter = new Cas20ProxyReceivingTicketValidationFilter();
             validationFilter.init(new WrappedFilterConfig(params));
             validationFilter.setRedirectAfterValidation(false);
-        }
-        catch (ServletException e)
-        {
+        } catch (ServletException e) {
             LOGGER.error("Failed to initialize internal validation filter!", e);
             validationFilter = null;
         }
     }
 
-    public Principal getUser(final HttpServletRequest request, final HttpServletResponse response)
-    {
-        // First, check to see if this session has already been authenticated during a previous request.
+    public Principal getUser(final HttpServletRequest request, final HttpServletResponse response) {
+        // First, check to see if this session has already been authenticated
+        // during a previous request.
         final HttpSession session = request.getSession(false);
 
-        if (session != null)
-        {
+        if (session != null) {
             Principal existingUser = getUserFromSessionOrAssertion(request, response);
-            if (existingUser != null) return existingUser;
+            if (existingUser != null)
+                return existingUser;
         }
-        if (response != null)
-        {
-            try
-            {
-                validationFilter.doFilter(request, response, new FilterChain()
-                {
+        if (response != null) {
+            try {
+                validationFilter.doFilter(request, response, new FilterChain() {
                     @Override
-                    public void doFilter(ServletRequest request, ServletResponse response)
-                            throws IOException, ServletException
-                    {
+                    public void doFilter(ServletRequest request, ServletResponse response) throws IOException, ServletException {
                     }
                 });
                 return getUserFromSessionOrAssertion(request, response);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 LOGGER.debug("Call to internal validation filter failed", e);
             }
         }
         return null;
     }
 
-    public boolean logout(final HttpServletRequest request, final HttpServletResponse response)
-            throws AuthenticatorException
-    {
+    public boolean logout(final HttpServletRequest request, final HttpServletResponse response) throws AuthenticatorException {
         final HttpSession session = request.getSession();
         final Principal p = (Principal) session.getAttribute(LOGGED_IN_KEY);
 
-        if (p != null)
-        {
+        if (p != null) {
             LOGGER.debug("Logging out [{}] from CAS.", p.getName());
         }
 
@@ -126,35 +112,28 @@ public final class Jira7CasAuthenticator extends JiraSeraphAuthenticator
         return super.logout(request, response);
     }
 
-    private Principal getUserFromSessionOrAssertion(final HttpServletRequest request,
-            final HttpServletResponse response)
-    {
-        // First, check to see if this session has already been authenticated during a previous request.
+    private Principal getUserFromSessionOrAssertion(final HttpServletRequest request, final HttpServletResponse response) {
+        // First, check to see if this session has already been authenticated
+        // during a previous request.
         final HttpSession session = request.getSession(false);
-        if (session != null)
-        {
+        if (session != null) {
             Principal existingUser = getUserFromSession(request);
-            if (existingUser != null)
-            {
+            if (existingUser != null) {
                 LOGGER.debug("Session found; user already logged in.");
                 return existingUser;
             }
 
             final Assertion assertion = (Assertion) session.getAttribute(AbstractCasFilter.CONST_CAS_ASSERTION);
-            if (assertion != null)
-            {
+            if (assertion != null) {
                 final String username = assertion.getPrincipal().getName();
                 final Principal user = getUser(username);
 
-                if (user != null)
-                {
+                if (user != null) {
                     putPrincipalInSessionContext(request, user);
                     getElevatedSecurityGuard().onSuccessfulLoginAttempt(request, username);
                     LoginReason.OK.stampRequestResponse(request, response);
                     LOGGER.debug("Logging in [{}] from CAS.", username);
-                }
-                else
-                {
+                } else {
                     LOGGER.debug("Failed logging [{}] from CAS.", username);
                     getElevatedSecurityGuard().onFailedLoginAttempt(request, username);
                 }
@@ -164,36 +143,30 @@ public final class Jira7CasAuthenticator extends JiraSeraphAuthenticator
         return null;
     }
 
-    private class WrappedFilterConfig implements FilterConfig
-    {
+    private class WrappedFilterConfig implements FilterConfig {
         private final Map<String, String> params;
 
-        public WrappedFilterConfig(Map<String, String> params)
-        {
+        public WrappedFilterConfig(Map<String, String> params) {
             this.params = ImmutableMap.copyOf(params);
         }
 
         @Override
-        public String getFilterName()
-        {
+        public String getFilterName() {
             return null;
         }
 
         @Override
-        public ServletContext getServletContext()
-        {
+        public ServletContext getServletContext() {
             return ServletContextProvider.getServletContext();
         }
 
         @Override
-        public String getInitParameter(String name)
-        {
+        public String getInitParameter(String name) {
             return params.get(name);
         }
 
         @Override
-        public Enumeration<String> getInitParameterNames()
-        {
+        public Enumeration<String> getInitParameterNames() {
             return Iterators.asEnumeration(params.keySet().iterator());
         }
     }
