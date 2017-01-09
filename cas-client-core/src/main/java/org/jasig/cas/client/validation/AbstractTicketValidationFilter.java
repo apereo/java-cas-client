1e7b6d0d5fb2742b6e83cf435b7d6aa4302be96a
Pisut Hutton <pisuth@ziontech.com>
2017-01-09 15:57:45 -0800

    Revert recent changes

--- cas-client-core/src/main/java/org/jasig/cas/client/validation/AbstractTicketValidationFilter.java
+++ cas-client-core/src/main/java/org/jasig/cas/client/validation/AbstractTicketValidationFilter.java
@@ -242,11 +242,8 @@ public abstract class AbstractTicketValidationFilter extends AbstractCasFilter {
                     response.sendRedirect(constructServiceUrl(request, response));
                     return;
                 }
-            } catch (final TicketValidationException | IOException | RuntimeException e) {
+            } catch (final TicketValidationException e) {
                 //We don't want to fail on the server so move forward by reloading.
-                if ((e instanceof RuntimeException) && !(e.getCause() instanceof IOException)) {
-                    throw (RuntimeException) e;
-                }
                 logger.debug(e.getMessage(), e);
 
                 onFailedValidation(request, response);
