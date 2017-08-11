d5caabf869cd1069ba54087ac45377c0863cf400
Pengxuan Men <pengxuan.men@gmail.com>
2017-08-11 17:25:38 +0800

    Fix issue when parse uri with encode qual sign or and sign.

--- cas-client-core/src/main/java/org/jasig/cas/client/util/URIBuilder.java
+++ cas-client-core/src/main/java/org/jasig/cas/client/util/URIBuilder.java
@@ -112,13 +112,13 @@ public final class URIBuilder {
             final Charset utf8 = Charset.forName("UTF-8");
             if (query != null && !query.isEmpty()) {
                 final List<BasicNameValuePair> list = new ArrayList<BasicNameValuePair>();
-                final String queryValue = URLDecoder.decode(query, utf8.name());
-                final String[] parametersArray = queryValue.split("&");
+                final String[] parametersArray = query.split("&");
 
                 for (final String parameter : parametersArray) {
                     final String[] parameterCombo = parameter.split("=");
                     if (parameterCombo.length == 2) {
-                        list.add(new BasicNameValuePair(parameterCombo[0], parameterCombo[1]));
+                        final String queryValue = URLDecoder.decode(parameterCombo[1], utf8.name());
+                        list.add(new BasicNameValuePair(parameterCombo[0], queryValue));
                     }
                 }
                 return list;
