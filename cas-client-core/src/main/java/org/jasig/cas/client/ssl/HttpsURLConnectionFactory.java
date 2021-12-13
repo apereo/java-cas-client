0af2a862ebd0fbf4523a861cc1dd8a3602833c09
Misagh <mm1844@gmail.com>
2021-12-13 21:45:03 +0400

    support ssl config to skip failures optionally

--- cas-client-core/src/main/java/org/jasig/cas/client/ssl/HttpsURLConnectionFactory.java
+++ cas-client-core/src/main/java/org/jasig/cas/client/ssl/HttpsURLConnectionFactory.java
@@ -6,9 +6,9 @@
  * Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License.  You may obtain a
  * copy of the License at the following location:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -18,21 +18,34 @@
  */
 package org.jasig.cas.client.ssl;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URLConnection;
-import java.security.KeyStore;
-import java.util.Properties;
-import javax.net.ssl.*;
 import org.jasig.cas.client.util.CommonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URLConnection;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.Properties;
 
 /**
  * An implementation of the {@link HttpURLConnectionFactory} whose responsible to configure
  * the underlying <i>https</i> connection, if needed, with a given hostname and SSL socket factory based on the
  * configuration provided. 
- * 
+ *
  * @author Misagh Moayyed
  * @since 3.3
  * @see #setHostnameVerifier(HostnameVerifier)
@@ -69,7 +82,7 @@ public final class HttpsURLConnectionFactory implements HttpURLConnectionFactory
 
     /**
      * Set the host name verifier for the https connection received.
-     * 
+     *
      * @see AnyHostnameVerifier
      * @see RegexHostnameVerifier
      * @see WhitelistHostnameVerifier
@@ -81,6 +94,34 @@ public final class HttpsURLConnectionFactory implements HttpURLConnectionFactory
     @Override
     public HttpURLConnection buildHttpURLConnection(final URLConnection url) {
         return this.configureHttpsConnectionIfNeeded(url);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final HttpsURLConnectionFactory that = (HttpsURLConnectionFactory) o;
+
+        if (!hostnameVerifier.equals(that.hostnameVerifier)) {
+            return false;
+        }
+        if (!sslConfiguration.equals(that.sslConfiguration)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = hostnameVerifier.hashCode();
+        result = 31 * result + sslConfiguration.hashCode();
+        return result;
     }
 
     /**
@@ -98,11 +139,17 @@ public final class HttpsURLConnectionFactory implements HttpURLConnectionFactory
                 httpsConnection.setSSLSocketFactory(socketFactory);
             }
 
-            if (this.hostnameVerifier != null) {
+            if (isIgnoreSslFailures()) {
+                httpsConnection.setHostnameVerifier(new AnyHostnameVerifier());
+            } else if (this.hostnameVerifier != null) {
                 httpsConnection.setHostnameVerifier(this.hostnameVerifier);
             }
         }
         return (HttpURLConnection) conn;
+    }
+
+    private boolean isIgnoreSslFailures() {
+        return this.sslConfiguration.getProperty("ignoreSslFailures", "false").equalsIgnoreCase("true");
     }
 
     /**
@@ -120,8 +167,22 @@ public final class HttpsURLConnectionFactory implements HttpURLConnectionFactory
      */
     private SSLSocketFactory createSSLSocketFactory() {
         InputStream keyStoreIS = null;
+
         try {
             final SSLContext sslContext = SSLContext.getInstance(this.sslConfiguration.getProperty("protocol", "SSL"));
+            if (isIgnoreSslFailures()) {
+                final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+                    public void checkClientTrusted(final X509Certificate[] certs, final String authType) {
+                    }
+                    public void checkServerTrusted(final X509Certificate[] certs, final String authType) {
+                    }
+                }};
+                sslContext.init(null, trustAllCerts, new SecureRandom());
+                return sslContext.getSocketFactory();
+            }
 
             if (this.sslConfiguration.getProperty("keyStoreType") != null) {
                 final KeyStore keyStore = KeyStore.getInstance(this.sslConfiguration.getProperty("keyStoreType"));
@@ -131,9 +192,8 @@ public final class HttpsURLConnectionFactory implements HttpURLConnectionFactory
                         keyStore.load(keyStoreIS, this.sslConfiguration.getProperty("keyStorePass").toCharArray());
                         LOGGER.debug("Keystore has {} keys", keyStore.size());
                         final KeyManagerFactory keyManager = KeyManagerFactory.getInstance(this.sslConfiguration
-                                .getProperty("keyManagerType", "SunX509"));
-                        keyManager.init(keyStore, this.sslConfiguration.getProperty("certificatePassword")
-                                .toCharArray());
+                            .getProperty("keyManagerType", "SunX509"));
+                        keyManager.init(keyStore, this.sslConfiguration.getProperty("certificatePassword").toCharArray());
                         sslContext.init(keyManager.getKeyManagers(), null, null);
                         return sslContext.getSocketFactory();
                     }
@@ -146,26 +206,6 @@ public final class HttpsURLConnectionFactory implements HttpURLConnectionFactory
             CommonUtils.closeQuietly(keyStoreIS);
         }
         return null;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        final HttpsURLConnectionFactory that = (HttpsURLConnectionFactory) o;
-
-        if (!hostnameVerifier.equals(that.hostnameVerifier)) return false;
-        if (!sslConfiguration.equals(that.sslConfiguration)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = hostnameVerifier.hashCode();
-        result = 31 * result + sslConfiguration.hashCode();
-        return result;
     }
 
     private void writeObject(final ObjectOutputStream out) throws IOException {
